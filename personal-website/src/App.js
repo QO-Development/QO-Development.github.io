5d27d870068311635c059844959404414b532e8f
Josh Hawthorne <johawth31@gmail.com>
2021-02-12 15:23:00 -0800

    Working on nav bar

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/nav';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <head>
+          {/* <!-- Required meta tags --> */}
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          {/* <!-- Bootstrap CSS --> */}
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
+          <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+          <title>Hawthorne Hacks</title>
+        </head>
+
+        <Nav />
+
+        <body>
+          <h1>Hello, world! Place the rest of your body code here</h1>
+
+          {/* <!-- Bootstrap Bundle with Popper --> */}
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+        </body>
+      </>
+  
   );
 }
 
---
+++ personal-website/src/components/nav/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './nav.css';
+
+class Nav extends React.Component {
+
+  render() {
+
+    return(
+      
+      <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#e3f2fd'}}>
+
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Hawthorne Hacks</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+              <li class="nav-item">
+                {/* <a class="nav-link" href="#">Twitter Icon</a> */}
+                <i class="fas fa-cloud"></i>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Github Icon</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">LinkedIn Icon</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;  
