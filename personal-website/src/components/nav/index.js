5111a7d85fb3a9d56329208a4199bb69553d68c8
Josh Hawthorne <johawth31@gmail.com>
2021-02-12 16:59:01 -0800

    Bug fixes in react bootstrap

--- personal-website/src/components/nav/index.js
+++ personal-website/src/components/nav/index.js
@@ -3,46 +3,29 @@ import './nav.css';
 import twitterIcon from '../../assets/twitter.png';
 import githubIcon from '../../assets/github.png';
 import linkedinIcon from '../../assets/linkedin.png';
+import {Navbar, Nav} from 'react-bootstrap';
 
-class Nav extends React.Component {
+
+class NavHeader extends React.Component {
 
   render() {
-
     return(
-      
-      <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#e3f2fd'}}>
-
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Hawthorne Hacks</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Projects</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Contact</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://twitter.com/hawtcode"><img src={twitterIcon}></img></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://github.com/QO-Development"><img src={githubIcon}></img></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://www.linkedin.com/in/joshuahawthorne/"><img src={linkedinIcon}></img></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar className="colorNav" expand="lg">
+        <Navbar.Brand href="/"><h3>Hawthorne Hacks</h3></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="linksContainer">
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/">Projects</Nav.Link>
+            <Nav.Link href="/">Contact</Nav.Link>
+            <Nav.Link href="https://twitter.com/hawtcode"><img alt="twitter icon" src={twitterIcon}></img></Nav.Link>
+            <Nav.Link href="https://github.com/QO-Development"><img alt="github icon" src={githubIcon}></img></Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/joshuahawthorne/"><img alt="linkedIn icon" src={linkedinIcon}></img></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
 
-export default Nav;  
+export default NavHeader;  
