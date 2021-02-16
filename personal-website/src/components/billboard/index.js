3293496e771fa4ba1188ad313432bd8f27e7c780
Josh Hawthorne <johawth31@gmail.com>
2021-02-15 18:58:24 -0800

    Working on typewriter animation

--- personal-website/src/components/billboard/index.js
+++ personal-website/src/components/billboard/index.js
@@ -9,22 +9,25 @@ class Billboard extends React.Component {
         <Row>
           <Col style={{border: '2px red solid'}} md={12}>
             <div className="typewriter">
-              <h4> func make(coffee string) Product &#123; </h4>
-              <h4> if caffeinated &#123; </h4>
-              <h4> product := hack() </h4>
-              <h4> return product </h4>
-              <h4> &#125; </h4>
-              <h4> else &#123; </h4>
-              <h4> consume(coffee)</h4>
-              <h4> product := make(coffee)</h4>
-              <h4> return product</h4>
-              <h4> &#125; </h4>
+              <p className="type"> func make(coffee string) Product &#123; </p>
+              <p className="type"> if caffeinated &#123; </p>
+              <p className="type"> product := hack() </p>
+              <p className="type"> return product </p>
+              <p className="type"> &#125; </p>
+              <p className="type"> else &#123; </p>
+              <p className="type"> consume(coffee)</p>
+              <p className="type"> product := make(coffee)</p>
+              <p className="type"> return product</p>
+              <p className="type"> &#125; </p>
             </div>
           </Col>
-          <Col style={{border: '2px red solid'}} md={4}> <h4>Professional photo goes here</h4></Col>
+          <Col style={{border: '2px red solid'}} md={4}> 
+            <h2>About</h2>
+            <h4>Professional photo goes here</h4>
+          </Col>
           <Col style={{border: '2px red solid'}} md={8}>
             <p>Hello! My name is Josh Hawthorne and I'm a Full Stack Software Engineer based in the Los Angeles/Orange County area of Southern California.</p>
-            <p>My current tech stack includes React, React Native, Golang, PostgreSQL, Google Cloud Platform, and iOS development in Swift. I am passionate about creating great products that add value to people's lives with the latest and greatest technology. I am a nerd, and I like to build cool stuff. </p>
+            <p>My current tech stack includes React, React Native, Golang, PostgreSQL, Google Cloud Platform, and iOS development in Swift. I am passionate about creating great products that add value to people's lives with the latest and greatest technology. In other words, I like to build cool stuff. </p>
             <p>When I'm not building things you can probably find me on a board of some kind (surf, SUP, or snow), golfing, strength training, reading, or playing my guitar.</p>
             <p>I graduated from the University of Minnesota in 2010 with a BS in Electrical Engineering, and a minor in Business Management. I spent a semester studying abroad in Australia on the Gold Coast, and ever since inernational travel has been a great love of mine.</p>
             <p>After school, I spent some time working in the oilfield as a field engineer. In addition to Australia, I've also lived in Saudi Arabia, Russia, and Mexico. My Spanish is aceptable, pero no puedo hablar como nativo. I couldn't tell you more than 10 words in Arabic if my life depended on it. And the only thing I know how to do in Russian is curse (thanks Russian roughnecks).</p>
@@ -32,6 +35,55 @@ class Billboard extends React.Component {
             <p>Feel free to poke around, drop me a line, or check out examples of my past work below. Thanks for stopping by!</p>
           </Col>
         </Row>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <h3>Vertlas</h3>
+            <h3>Todayte</h3>
+            <h3>Backup Genius</h3>
+            <h3>And more...</h3>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h4>Want to see more public examples of my work? Check out my <a href="/">Github</a></h4>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h2>Experience and Education</h2>
+            <p>Make a nicely formatted resume here, and make it hide by default</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h2>Skills</h2>
+            <p>List all your skills here with icons...</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h2>Get in Touch</h2>
+            <p>Got something you'd like to talk about? Please feel free to contact me at `email icon here` josh@hawthornehacks.com</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <a href="/">Twitter</a> <a href="/">LinkedIn</a> <a href="/">GitHub</a>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <p>&copy; 2021 Hawthorne Hacks</p>
+          </Col>
+        </Row>
+
       </Container>
     );
   }
